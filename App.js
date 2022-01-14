63af09f7ca79671b55790cb29393de43aa27e273
kenneth34 <kendor34@yahoo.com.sg>
2022-01-14 21:39:34 +0800

    Task 7

--- App.js
+++ App.js
@@ -5,18 +5,34 @@ export default function App() {
   //loading is a variable, with initial value true
   // setloading is a function to set the value for loading variable to any boolean value
   // useState(true) is a function that initialise state of the loading variable to true
-  const [loading, setloading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [arrival, setArrival] = useState("");
   const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
 
   function loadBusStopData() {
+    setLoading(true);
     fetch(BUSSTOP_URL)
     .then((response) => {
-      return response.json();
+      return response.json()
     })
     .then((responseData) => {
+      console.log("Original data:");
       console.log(responseData);
+
+      const myBus = responseData.services.filter(
+        (bus) => bus.no === "15"
+        ) [0];
+        console.log("My bus:");
+        setArrival(myBus.next.time);
+        setLoading(false);
+
     })
   }
+
+useEffect(() => {
+  const interval = setInterval(loadBusStopData, 1000);
+  return () => clearInterval(interval);
+}, []);
 
   useEffect(() => {
     loadBusStopData();
@@ -24,10 +40,11 @@ export default function App() {
   
   return (
     <View style={styles.container}>
-      <Text>Bus Arrival Time:</Text>
-      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator size="large" /> : 'loaded'}</Text>
+      <Text style={styles.info}>Bus Arrival Time:</Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? <ActivityIndicator size="large" color= "blue"/> : arrival}</Text>
       <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Refresh</Text>
+        <Text style={styles.buttonText}>Refresh!</Text>
       </TouchableOpacity>
     
     </View>
@@ -48,5 +65,15 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 10,
   },
+  arrivalTime: {
+    color: 'orange',
+    fontSize: 25,
+    margin: 20,
+  },
+  info: {
+    color: 'blue',
+    fontSize: 30,
+  },
+  
   
 });
