23e56686a9ebd0b6c5fe9c4eff5749a6bb15bef5
kenneth34 <kendor34@yahoo.com.sg>
2022-01-19 08:10:23 +0800

    2 diff time

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-import { useState, useEffect, } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator, } from 'react-native';
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
   //loading is a variable, with initial value true
@@ -11,37 +17,35 @@ export default function App() {
   const PSI_URL = "https://api.data.gov.sg/v1/environment/psi";
   const [Refreshing, setRefreshing] = useState("");
   const [duration, setDuration] = useState("");
+  const BusNo = 15;
+  const BusStopNo = 83139;
 
   function loadBusStopData() {
     setLoading(true);
     fetch(BUSSTOP_URL)
-    .then((response) => {
-      return response.json()
-    })
-    .then((responseData) => {
-      console.log("Original data:");
-      console.log(responseData);
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log("Original data:");
+        console.log(responseData);
 
-      const myBus = responseData.services.filter(
-        (bus) => bus.no === "15"
-        ) [0];
+        const myBus = responseData.services.filter((bus) => bus.no === "15")[0];
         console.log("My bus:");
         setArrival(myBus.next.time);
-        setDuration(myBus.next2.time);
+        setDuration(myBus.next2.duration_ms);
         setLoading(false);
-        
+
         const date = new Date(myBus.next.time);
         const [hour, minutes, seconds] = [
-          ("0", + date.getHours()).slice(-2),
-          ("0", + date.getMinute()).slice(-2),
-          ("0", + date.getSeconds()).slice(-2),
+          ("0", +date.getHours()).slice(-2),
+          ("0", +date.getMinute()).slice(-2),
+          ("0", +date.getSeconds()).slice(-2),
         ];
         setArrivalTime([hour, ":", minutes, ":", seconds]);
-
-    })
-  }
-
-  
+        setDuration(myBus.next2.duration_ms);
+      });
+    }    
 
   const millistoMinutesAndSeconds = (millis) => {
     var minutes = Math.floor(millis / 60000);
@@ -49,61 +53,64 @@ export default function App() {
     return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
     //return `${minutes}:${(seconds < 10 ? "0" : "")}${seconds}`;
   };
-
  
-const BusNo = "15"
+//const onRefresh = () => {
+  ///setRefreshing(true);
+  //setArrival(myBus.next.time);
+  //setDuration(myBus.next2.time);
+  //setRefreshing(false);
+//};
 
-useEffect(() => {
-  const interval = setInterval(loadBusStopData, 10000);
-  return () => clearInterval(interval);
-}, []);
+  useEffect(() => {
+    const interval = setInterval(loadBusStopData, 10000);
+    return () => clearInterval(interval);
+  }, []);
 
   useEffect(() => {
     loadBusStopData();
   }, []);
-  
+
   return (
     <View style={styles.container}>
-      <Text style={styles.info}>Bus No : 15 </Text>
-      <Text style={styles.info}>Bus Stop : 83139</Text>
+      <Text style={styles.info}>Bus No : {BusNo} </Text>
+      <Text style={styles.info}>Bus Stop : {BusStopNo} </Text>
       <Text style={styles.info}>Bus Arrival Time:</Text>
       <Text style={styles.arrivalTime}>
-        {loading ? <ActivityIndicator size="large" color= "blue"/> : arrival}</Text>
-        <Text style={styles.info}>Next Bus:</Text>  
-        <Text style={styles.arrivalTime}>
-        {loading ? <ActivityIndicator size="large" color= "blue"/> : duration}</Text>
+        {loading ? <ActivityIndicator size="large" color="blue" /> : arrival}
+      </Text>
+      <Text style={styles.info}>Next Bus:</Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? <ActivityIndicator size="large" color="blue" /> : duration}
+      </Text>
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Refresh!</Text>
       </TouchableOpacity>
-    
     </View>
   );
-  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  
+
   button: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
     padding: 10,
     borderRadius: 10,
     margin: 10,
   },
   arrivalTime: {
-    color: 'orange',
+    color: "orange",
     fontSize: 25,
     margin: 20,
   },
   info: {
-    color: 'blue',
+    color: "blue",
     fontSize: 30,
     margin: 10,
   },
-  
-  
 });
